573569e9045031d1cb8262d9a7f38e9849844fb8
Muhammad Radya Iftikhar <radyaiftikhar@gmail.com>
2024-03-09 23:36:26 +0700

    ubah nama env

--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: process.env.VUE_FIREBASE_KEY,
+    apiKey: process.env.VUE_APP_API,
     authDomain: "chatwoe.firebaseapp.com",
     projectId: "chatwoe",
     storageBucket: "chatwoe.appspot.com",
